844fbba92386c712da1ba0a900160aa91ea2f880
J4JEDAH <maildev3d@gmail.com>
2021-07-01 02:13:37 +0100

    Handled SIGTERM requests from heroku

--- server.js
+++ server.js
@@ -39,3 +39,10 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
   });
 });
+
+process.on('SIGTERM', () => {
+  console.log(`SIGTERM RECIEVED... shutting down gracefully.`);
+  server.close(() => {
+    console.log('Process terminated');
+  });
+});
