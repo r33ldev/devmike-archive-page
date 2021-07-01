68b7de52afa07f62b58b8678f85a95772b821939
J4JEDAH <adebisienads@gmail.com>
2021-07-01 14:59:54 +0100

    Really improve stripe payment with a webhook

--- controllers/verificationController.js
+++ controllers/verificationController.js
@@ -4,7 +4,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const catchAsync = require('../utils/catchAsync');
 const Users = require('../models/userModel');
 
-exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+exports.session = catchAsync(async (req, res, next) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     success_url: `${req.protocol}://${req.get('host')}`,
@@ -29,7 +29,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 });
 
 const createVerificationSession = async (session) => {
-  const user = await Users.session.customer_email.id;
+  const user = session.customer_email.id;
   await Users.findByIdAndUpdate(user, {
     verified: true,
   });
